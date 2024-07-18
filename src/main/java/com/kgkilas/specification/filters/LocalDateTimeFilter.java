0d332f95a7f1b920c00e6e84f3def54f492bb25a
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-07-19 01:19:30 +0300

    Implementation of filter for LocalDateTime values.

---
+++ src/main/java/com/kgkilas/specification/filters/LocalDateTimeFilter.java
@@ -0,0 +1,33 @@
+package com.kgkilas.specification.filters;
+
+import java.io.Serial;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeFilter extends RangeFilter<LocalDateTime> {
+    @Serial
+    private static final long serialVersionUID = 7L;
+
+    public LocalDateTimeFilter() {
+    }
+
+    public LocalDateTimeFilter(LocalDateTimeFilter filter) {
+        super(filter);
+    }
+
+    public LocalDateTimeFilter copy() {
+        return new LocalDateTimeFilter(this);
+    }
+
+    @Override
+    public LocalDateTimeFilter setGreaterThanOrEqual(LocalDateTime equals) {
+
+        super.setGreaterThanOrEqual(equals.withHour(0).withMinute(0));
+        return this;
+    }
+
+    @Override
+    public LocalDateTimeFilter setLessThanOrEqual(LocalDateTime equals) {
+        super.setLessThanOrEqual(equals.withHour(23).withMinute(59));
+        return this;
+    }
+}
