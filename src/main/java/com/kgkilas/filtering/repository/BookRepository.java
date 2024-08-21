50ccafdc46ea5b1a08d92979e0041c5f97786823
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-08-21 22:34:58 +0300

    Seperate project into two in order to distinct library from rest api for testing purposes.

--- src/main/java/com/kgkilas/filtering/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kgkilas.filtering.repository;
-
-import com.kgkilas.filtering.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-}
--- src/main/java/com/kgkilas/filtering/util/Constants.java
+++
@@ -1,4 +0,0 @@
-package com.kgkilas.filtering.util;
-
-public class Constants {
-}
