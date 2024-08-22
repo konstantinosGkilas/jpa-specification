a624c0017ad9a6a7d6c687e6ed99ef63cbb1fa81
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-08-23 02:25:55 +0300

    Minor cleanup in order to support changes in GenericSpecification + addition of java docs for clarity.

--- rest/src/main/java/com/gkilas/filtering/rest/specification/BookSpecification.java
+++ rest/src/main/java/com/gkilas/filtering/rest/specification/BookSpecification.java
@@ -5,41 +5,46 @@ import com.gkilas.filtering.rest.domain.Author_;
 import com.gkilas.filtering.rest.domain.Book;
 import com.gkilas.filtering.rest.domain.Book_;
 import org.springframework.data.jpa.domain.Specification;
-import com.kgkilas.filtering.specification.*;
+import com.kgkilas.filtering.specification.CriteriaSpecification;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookSpecification extends CriteriaSpecification<Book> {
 
-        public static Specification<Book> buildBookSpecification(BookCriteria eventCriteria) {
-
-                return getSpecifications(eventCriteria);
+        public static Specification<Book> buildBookSpecification(BookCriteria bookCriteria) {
+                return getSpecifications(bookCriteria);
         }
 
         /**
          * Combines the specifications based on the provided BookCriteria.
          *
-         * @param eventCriteria The criteria for filtering events.
-         * @return A list of Specifications for the event entity.
+         * @param bookCriteria The criteria for filtering books.
+         * @return A combined Specification for the Book entity.
          */
-        protected static Specification<Book> getSpecifications(BookCriteria eventCriteria) {
-                CriteriaSpecification<Book> eventSpecification = new CriteriaSpecification<>();
-                List<Specification<Book>> joinSpecifications = buildSpecifications(eventSpecification, eventCriteria);
-                return combine(joinSpecifications);
+        protected static Specification<Book> getSpecifications(BookCriteria bookCriteria) {
+                CriteriaSpecification<Book> bookSpecification = new CriteriaSpecification<>();
+                List<Specification<Book>> joinSpecifications = buildSpecifications(bookSpecification, bookCriteria);
+                return combine(joinSpecifications);  // Ensure the combine method uses the new implementation
         }
 
         /**
          * Builds a list of specifications based on the provided criteria.
          *
          * @param bookCriteriaSpecification The generic specification builder.
-         * @param bookCriteria      The criteria for filtering events.
+         * @param bookCriteria      The criteria for filtering books.
          * @return A list of specifications.
          */
         private static List<Specification<Book>> buildSpecifications(CriteriaSpecification<Book> bookCriteriaSpecification, BookCriteria bookCriteria) {
                 List<Specification<Book>> specifications = new ArrayList<>();
+
+                // Build specifications for individual fields
                 addIfNotNull(specifications, bookCriteriaSpecification.buildSpecification(Book_.ID, bookCriteria.getBookId()));
                 addIfNotNull(specifications, bookCriteriaSpecification.buildSpecification(Book_.TITLE, bookCriteria.getTitle()));
+
+                // Use buildJoinSpecification for related entity fields
                 addIfNotNull(specifications, buildJoinSpecification(Book_.AUTHOR, bookCriteria.getAuthorName(), Author_.NAME));
+
                 return specifications;
         }
 }
