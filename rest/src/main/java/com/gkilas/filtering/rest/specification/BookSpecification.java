953daf92047c74e4c3fa90b97fed5b74ffc4a91b
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-08-22 23:46:26 +0300

    Implementaion of filtering using Jpa Metamodel and Specification, in order for the service to be future proof and easy to maintain.

--- rest/src/main/java/com/gkilas/filtering/rest/specification/BookSpecification.java
+++ rest/src/main/java/com/gkilas/filtering/rest/specification/BookSpecification.java
@@ -1,7 +1,9 @@
 package com.gkilas.filtering.rest.specification;
 
 import com.gkilas.filtering.rest.criteria.BookCriteria;
+import com.gkilas.filtering.rest.domain.Author_;
 import com.gkilas.filtering.rest.domain.Book;
+import com.gkilas.filtering.rest.domain.Book_;
 import org.springframework.data.jpa.domain.Specification;
 import com.kgkilas.filtering.specification.*;
 import java.util.ArrayList;
@@ -9,34 +11,35 @@ import java.util.List;
 
 public class BookSpecification extends CriteriaSpecification<Book> {
 
-public static Specification<Book> buildBookSpecification(BookCriteria eventCriteria) {
+        public static Specification<Book> buildBookSpecification(BookCriteria eventCriteria) {
 
-        return getSpecifications(eventCriteria);
+                return getSpecifications(eventCriteria);
         }
 
-/**
- * Combines the specifications based on the provided BookCriteria.
- *
- * @param eventCriteria The criteria for filtering events.
- * @return A list of Specifications for the event entity.
- */
-protected static Specification<Book> getSpecifications(BookCriteria eventCriteria) {
-        CriteriaSpecification<Book> eventSpecification = new CriteriaSpecification<>();
-        List<Specification<Book>> joinSpecifications = buildSpecifications(eventSpecification, eventCriteria);
-        return combine(joinSpecifications);
+        /**
+         * Combines the specifications based on the provided BookCriteria.
+         *
+         * @param eventCriteria The criteria for filtering events.
+         * @return A list of Specifications for the event entity.
+         */
+        protected static Specification<Book> getSpecifications(BookCriteria eventCriteria) {
+                CriteriaSpecification<Book> eventSpecification = new CriteriaSpecification<>();
+                List<Specification<Book>> joinSpecifications = buildSpecifications(eventSpecification, eventCriteria);
+                return combine(joinSpecifications);
         }
 
-/**
- * Builds a list of specifications based on the provided criteria.
- *
- * @param eventSpecification The generic specification builder.
- * @param eventCriteria      The criteria for filtering events.
- * @return A list of specifications.
- */
-private static List<Specification<Book>> buildSpecifications(CriteriaSpecification<Book> eventSpecification, BookCriteria eventCriteria) {
-        List<Specification<Book>> specifications = new ArrayList<>();
-        addIfNotNull(specifications, eventSpecification.buildSpecification("id", eventCriteria.getBookId()));
-        addIfNotNull(specifications, eventSpecification.buildSpecification("title", eventCriteria.getTitle()));
-        return specifications;
+        /**
+         * Builds a list of specifications based on the provided criteria.
+         *
+         * @param bookCriteriaSpecification The generic specification builder.
+         * @param bookCriteria      The criteria for filtering events.
+         * @return A list of specifications.
+         */
+        private static List<Specification<Book>> buildSpecifications(CriteriaSpecification<Book> bookCriteriaSpecification, BookCriteria bookCriteria) {
+                List<Specification<Book>> specifications = new ArrayList<>();
+                addIfNotNull(specifications, bookCriteriaSpecification.buildSpecification(Book_.ID, bookCriteria.getBookId()));
+                addIfNotNull(specifications, bookCriteriaSpecification.buildSpecification(Book_.TITLE, bookCriteria.getTitle()));
+                addIfNotNull(specifications, buildJoinSpecification(Book_.AUTHOR, bookCriteria.getAuthorName(), Author_.NAME));
+                return specifications;
         }
 }
