fee5d323d04fcaf0e1ca1defcc563cc5c7c805a9
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-08-23 22:51:42 +0300

    Addition of java docs and comments for clarity.

--- filter/src/main/java/com/kgkilas/filtering/filters/BaseFilter.java
+++ filter/src/main/java/com/kgkilas/filtering/filters/BaseFilter.java
@@ -8,94 +8,193 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * BaseFilter class is a generic filter for various field types. It supports equality, non-equality,
+ * specification checks, and inclusion/exclusion in lists.
+ *
+ * @param <T> the type of the field being filtered, which must be comparable.
+ */
 @Getter
 public class BaseFilter<T extends Comparable<? super T>> implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private FieldType<T> equals;
-    private FieldType<T> notEquals;
-    private Boolean specified;
-    private List<FieldType<T>> in;
-    private List<FieldType<T>> notIn;
+    private FieldType<T> equals;       // Holds the value to be checked for equality.
+    private FieldType<T> notEquals;    // Holds the value to be checked for inequality.
+    private Boolean specified;         // Indicates whether the field should be specified (not null).
+    private List<FieldType<T>> in;     // List of values for 'in' filtering.
+    private List<FieldType<T>> notIn;  // List of values for 'not in' filtering.
 
-    public BaseFilter() {}
+    /**
+     * Default constructor for BaseFilter.
+     */
+    public BaseFilter() {
+        // Default constructor
+    }
 
+    /**
+     * Copy constructor for BaseFilter. Creates a deep copy of the given filter.
+     *
+     * @param filter the BaseFilter instance to be copied.
+     */
     public BaseFilter(BaseFilter<T> filter) {
         this.equals = filter.equals;
         this.notEquals = filter.notEquals;
         this.specified = filter.specified;
-        this.in = (filter.in == null) ? null : new ArrayList<>(filter.in);
-        this.notIn = (filter.notIn == null) ? null : new ArrayList<>(filter.notIn);
+        this.in = copyList(filter.in);
+        this.notIn = copyList(filter.notIn);
     }
 
+    /**
+     * Creates a copy of the current BaseFilter instance.
+     *
+     * @return a new BaseFilter instance that is a copy of the current instance.
+     */
     public BaseFilter<T> copy() {
         return new BaseFilter<>(this);
     }
 
+    /**
+     * Sets the value to be checked for equality.
+     *
+     * @param equals the value to be set for equality check.
+     * @return the current instance of BaseFilter with updated equality value.
+     */
     public BaseFilter<T> setEquals(T equals) {
         this.equals = new FieldType<>(equals);
         return this;
     }
 
+    /**
+     * Sets the value to be checked for inequality.
+     *
+     * @param notEquals the value to be set for inequality check.
+     * @return the current instance of BaseFilter with updated inequality value.
+     */
     public BaseFilter<T> setNotEquals(T notEquals) {
         this.notEquals = new FieldType<>(notEquals);
         return this;
     }
 
+    /**
+     * Sets whether the field should be specified (not null).
+     *
+     * @param specified true if the field should be specified, false otherwise.
+     * @return the current instance of BaseFilter with updated specified status.
+     */
     public BaseFilter<T> setSpecified(Boolean specified) {
         this.specified = specified;
         return this;
     }
 
+    /**
+     * Sets a list of values for 'in' filtering.
+     *
+     * @param inValues the list of values to be set for 'in' filtering.
+     * @return the current instance of BaseFilter with updated 'in' values.
+     */
     public BaseFilter<T> setIn(List<T> inValues) {
-        if (inValues != null) {
-            this.in = new ArrayList<>();
-            for (T value : inValues) {
-                this.in.add(new FieldType<>(value));
-            }
-        }
+        this.in = convertListToFieldType(inValues);
         return this;
     }
 
+    /**
+     * Sets a list of values for 'not in' filtering.
+     *
+     * @param notInValues the list of values to be set for 'not in' filtering.
+     * @return the current instance of BaseFilter with updated 'not in' values.
+     */
     public BaseFilter<T> setNotIn(List<T> notInValues) {
-        if (notInValues != null) {
-            this.notIn = new ArrayList<>();
-            for (T value : notInValues) {
-                this.notIn.add(new FieldType<>(value));
-            }
-        }
+        this.notIn = convertListToFieldType(notInValues);
         return this;
     }
 
+    /**
+     * Compares this BaseFilter to another object for equality.
+     *
+     * @param o the object to compare to.
+     * @return true if the objects are equal, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BaseFilter<?> filter = (BaseFilter<?>) o;
-        return Objects.equals(equals, filter.equals) &&
-                Objects.equals(notEquals, filter.notEquals) &&
-                Objects.equals(specified, filter.specified) &&
-                Objects.equals(in, filter.in) &&
-                Objects.equals(notIn, filter.notIn);
+        boolean isEqual = false;
+        if (this == o) {
+            isEqual = true;
+        } else if (o != null && getClass() == o.getClass()) {
+            BaseFilter<?> that = (BaseFilter<?>) o;
+            isEqual = Objects.equals(this.equals, that.equals) &&
+                    Objects.equals(this.notEquals, that.notEquals) &&
+                    Objects.equals(this.specified, that.specified) &&
+                    Objects.equals(this.in, that.in) &&
+                    Objects.equals(this.notIn, that.notIn);
+        }
+        return isEqual;
     }
 
+    /**
+     * Generates a hash code for this BaseFilter.
+     *
+     * @return the hash code of this BaseFilter.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(equals, notEquals, specified, in, notIn);
     }
 
+    /**
+     * Returns a string representation of this BaseFilter, detailing the filter conditions.
+     *
+     * @return a string representation of the filter.
+     */
     @Override
     public String toString() {
-        return getFilterName() + " [" +
-                (equals != null ? "equals=" + equals + ", " : "") +
-                (notEquals != null ? "notEquals=" + notEquals + ", " : "") +
-                (specified != null ? "specified=" + specified + ", " : "") +
-                (in != null ? "in=" + in + ", " : "") +
-                (notIn != null ? "notIn=" + notIn : "") + "]";
+        StringBuilder sb = new StringBuilder(getFilterName() + " [");
+        if (equals != null) sb.append("equals=").append(equals).append(", ");
+        if (notEquals != null) sb.append("notEquals=").append(notEquals).append(", ");
+        if (specified != null) sb.append("specified=").append(specified).append(", ");
+        if (in != null) sb.append("in=").append(in).append(", ");
+        if (notIn != null) sb.append("notIn=").append(notIn);
+
+        if (sb.charAt(sb.length() - 2) == ',') {
+            sb.deleteCharAt(sb.length() - 2); // Remove trailing comma
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
+    /**
+     * Returns the simple class name of the filter for the toString method.
+     *
+     * @return the simple class name of this filter.
+     */
     protected String getFilterName() {
         return getClass().getSimpleName();
+    }
+
+    /**
+     * Converts a list of values into a list of FieldType objects.
+     *
+     * @param values the list of values to convert.
+     * @return a list of FieldType objects.
+     */
+    private List<FieldType<T>> convertListToFieldType(List<T> values) {
+        List<FieldType<T>> fieldTypeList = new ArrayList<>();
+        if (values != null) {
+            for (T value : values) {
+                fieldTypeList.add(new FieldType<>(value));
+            }
+        }
+        return fieldTypeList;
+    }
+
+    /**
+     * Creates a shallow copy of the given list of FieldType objects.
+     *
+     * @param list the list to copy.
+     * @return a shallow copy of the given list.
+     */
+    private List<FieldType<T>> copyList(List<FieldType<T>> list) {
+        return (list != null) ? new ArrayList<>(list) : null;
     }
 }
--- filter/src/main/java/com/kgkilas/filtering/filters/RangeFilter.java
+++ filter/src/main/java/com/kgkilas/filtering/filters/RangeFilter.java
@@ -5,6 +5,13 @@ import lombok.Getter;
 import java.io.Serial;
 import java.util.Objects;
 
+/**
+ * RangeFilter is a generic filter class that allows filtering based on
+ * range criteria like greater than, less than, greater than or equal to,
+ * less than or equal to, and like conditions.
+ *
+ * @param <T> the type of the field to be filtered, which must be comparable.
+ */
 @Getter
 public class RangeFilter<T extends Comparable<? super T>> extends BaseFilter<T> {
     @Serial
@@ -16,8 +23,18 @@ public class RangeFilter<T extends Comparable<? super T>> extends BaseFilter<T> 
     private FieldType<T> lessThanOrEqual;
     private FieldType<T> like;
 
-    public RangeFilter() {}
+    /**
+     * Default constructor for RangeFilter.
+     */
+    public RangeFilter() {
+        // No initialization required for default constructor
+    }
 
+    /**
+     * Copy constructor for creating a new RangeFilter from an existing one.
+     *
+     * @param filter the existing RangeFilter to copy from.
+     */
     public RangeFilter(RangeFilter<T> filter) {
         super(filter);
         this.greaterThan = filter.greaterThan;
@@ -27,42 +44,81 @@ public class RangeFilter<T extends Comparable<? super T>> extends BaseFilter<T> 
         this.like = filter.like;
     }
 
+    /**
+     * Creates a copy of the current RangeFilter instance.
+     *
+     * @return a new RangeFilter instance with the same settings.
+     */
     @Override
     public RangeFilter<T> copy() {
         return new RangeFilter<>(this);
     }
 
+    /**
+     * Sets the greaterThan field with the specified value.
+     *
+     * @param greaterThan the value to set as greaterThan.
+     * @return the current RangeFilter instance for method chaining.
+     */
     public RangeFilter<T> setGreaterThan(T greaterThan) {
         this.greaterThan = new FieldType<>(greaterThan);
         return this;
     }
 
+    /**
+     * Sets the lessThan field with the specified value.
+     *
+     * @param lessThan the value to set as lessThan.
+     * @return the current RangeFilter instance for method chaining.
+     */
     public RangeFilter<T> setLessThan(T lessThan) {
         this.lessThan = new FieldType<>(lessThan);
         return this;
     }
 
+    /**
+     * Sets the greaterThanOrEqual field with the specified value.
+     *
+     * @param greaterThanOrEqual the value to set as greaterThanOrEqual.
+     * @return the current RangeFilter instance for method chaining.
+     */
     public RangeFilter<T> setGreaterThanOrEqual(T greaterThanOrEqual) {
         this.greaterThanOrEqual = new FieldType<>(greaterThanOrEqual);
         return this;
     }
 
+    /**
+     * Sets the lessThanOrEqual field with the specified value.
+     *
+     * @param lessThanOrEqual the value to set as lessThanOrEqual.
+     * @return the current RangeFilter instance for method chaining.
+     */
     public RangeFilter<T> setLessThanOrEqual(T lessThanOrEqual) {
         this.lessThanOrEqual = new FieldType<>(lessThanOrEqual);
         return this;
     }
 
+    /**
+     * Sets the like field with the specified value.
+     *
+     * @param like the value to set as like.
+     * @return the current RangeFilter instance for method chaining.
+     */
     public RangeFilter<T> setLike(T like) {
         this.like = new FieldType<>(like);
         return this;
     }
 
+    /**
+     * Compares this RangeFilter with another object for equality.
+     *
+     * @param o the object to compare with this RangeFilter.
+     * @return true if the objects are equal, otherwise false.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        RangeFilter<?> that = (RangeFilter<?>) o;
+        if (!(o instanceof RangeFilter<?> that) || !super.equals(o)) return false;
         return Objects.equals(greaterThan, that.greaterThan) &&
                 Objects.equals(lessThan, that.lessThan) &&
                 Objects.equals(greaterThanOrEqual, that.greaterThanOrEqual) &&
@@ -70,19 +126,31 @@ public class RangeFilter<T extends Comparable<? super T>> extends BaseFilter<T> 
                 Objects.equals(like, that.like);
     }
 
+    /**
+     * Returns the hash code value for this RangeFilter.
+     *
+     * @return the hash code value.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), greaterThan, lessThan, greaterThanOrEqual, lessThanOrEqual, like);
     }
 
+    /**
+     * Provides a string representation of this RangeFilter.
+     *
+     * @return the string representation of this RangeFilter.
+     */
     @Override
     public String toString() {
-        return super.toString() + " [" +
-                (greaterThan != null ? "greaterThan=" + greaterThan + ", " : "") +
-                (lessThan != null ? "lessThan=" + lessThan + ", " : "") +
-                (greaterThanOrEqual != null ? "greaterThanOrEqual=" + greaterThanOrEqual + ", " : "") +
-                (lessThanOrEqual != null ? "lessThanOrEqual=" + lessThanOrEqual + ", " : "") +
-                (like != null ? "like=" + like : "") +
-                "]";
+        StringBuilder sb = new StringBuilder(super.toString());
+        sb.append(" [");
+        if (greaterThan != null) sb.append("greaterThan=").append(greaterThan).append(", ");
+        if (lessThan != null) sb.append("lessThan=").append(lessThan).append(", ");
+        if (greaterThanOrEqual != null) sb.append("greaterThanOrEqual=").append(greaterThanOrEqual).append(", ");
+        if (lessThanOrEqual != null) sb.append("lessThanOrEqual=").append(lessThanOrEqual).append(", ");
+        if (like != null) sb.append("like=").append(like);
+        sb.append("]");
+        return sb.toString();
     }
 }
